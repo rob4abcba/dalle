04078e0d599c8e56b2506bd17ba82113d59edf98
rob <rob_abcba@yahoo.com>
2023-07-20 07:33:48 -0700

    Try className="text-3x1 font-bold underline" to test that tailwindcss is config correctly."

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Try className="text-3x1 font-bold underline" to Test that tailwindcss is configured correctly
 const App = () => {
   return <h1 className="text-3x1 font-bold underline">Hello world Yo</h1>;
 };
