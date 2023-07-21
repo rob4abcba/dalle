05f83618e495be27eba79a1bb62f2bbb9f998c7e
rob <rob_abcba@yahoo.com>
2023-07-21 06:04:47 -0700

    Build skeleton of CreatePost.jsx, which requires utils, which requires contants.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p
+          className="mt-2 text-[#666e75]
+            text-[16px] max-w-[500px]"
+        >
+          Create imaginative and visually stunning images through DALLE-E AI and
+          share them with the community
+        </p>
+      </div>
+      <form>
+        <div>
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="plush toy robot near a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
