35e75bbbea9af2a672498e1033d0df2bca4f5721
rob <rob_abcba@yahoo.com>
2023-07-22 10:40:16 -0700

    setForm in handleChange allows us to change name and prompt to the value we enter.  setForm in handleSurpriseMe sets prompt string to random sentence from our list of suggestions.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,8 +16,15 @@ const CreatePost = () => {
 
   const generateImage = () => {};
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
